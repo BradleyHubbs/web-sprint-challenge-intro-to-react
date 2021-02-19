a84315b008af476b8404f030cdf2e0f0e5970762
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2021-02-19 13:43:43 -0500

    react-sprint-project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import { render } from 'react-dom'
+import Characters from './components/Character'
 import './App.css';
 
-const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +11,35 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
+let baseURL = 'http://swapi.py4e.com/api/people/'
+
+const App = () => {
+  const [characterNames, setCharacterNames] = useState([])
+
+
+useEffect(() => {
+  axios
+  .get(baseURL)
+  .then((res) =>{
+    setCharacterNames(res.data.results.map(p => p))
+  })
+  .catch((err) => {
+    console.log('error')
+  })
+}, []);
+
+
+
+
+
+
+    return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
+      
+        <Characters characterNames={characterNames} />
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,54 @@
-// Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+export default function Characters(props) {
+    const { characterNames } = props
+
+    
+    const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    `;
+
+    const StyledChar = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    color: white;
+    background-color: black;
+    border: 10px blue;
+    margin: 8px;
+    padding: 8px;
+    border-style: solid;
+    border-color: pink;
+    border-radius: 25px;
+    `
+    
+
+
+    return(
+        <>
+        <StyledContainer>
+            {characterNames.map(p=> (
+                <>
+                <StyledChar>
+                <h3>{p.name}</h3>
+                <div>Birthyear: {p.birth_year} </div>
+                <div>Mass: {p.mass}KGs</div>
+                <div>Gender: {p.gender}</div>
+                <div>Hair Color: {p.hair_color}</div>
+                <div>Skin Color: {p.skin_color}</div>
+                <div>Eye Color: {p.eye_color}</div>
+                </StyledChar>
+                </>
+            ))}
+           
+        </StyledContainer>
+        </>
+    )
+}
